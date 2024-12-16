ed630c9a4458f9b47afb73b8f6a63e18076d0114
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2024-12-16 14:55:27 +0700

    mengimport fungsi dari code DetailMhsViewModel

--- app/src/main/java/com/example/meet9cruddengandb/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/meet9cruddengandb/ui/viewmodel/DetailMhsViewModel.kt
@@ -1,5 +1,21 @@
 package com.example.meet9cruddengandb.ui.viewmodel
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.meet9cruddengandb.data.entity.Mahasiswa
+import com.example.meet9cruddengandb.repository.RepositoryMhs
+import com.example.meet9cruddengandb.ui.navigation.DestinasiDetail
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
 class DetailMhsViewModel(
     savedStateHandle: SavedStateHandle,
     private val repositoryMhs: RepositoryMhs
