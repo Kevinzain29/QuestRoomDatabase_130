7d249728049e577a80a0937dc4a80e7598d5904d
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2024-12-16 14:48:35 +0700

    menambahkan class HomeViewModel untuk mengelola state "Home Mahasiswa"

--- app/src/main/java/com/example/meet9cruddengandb/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/meet9cruddengandb/ui/viewmodel/HomeMhsViewModel.kt
@@ -1,2 +1,34 @@
 package com.example.meet9cruddengandb.ui.viewmodel
 
+class HomeMhsViewModel(
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel() {
+    val homeUiState: StateFlow<HomeUiState> = repositoryMhs.getAllMahasiswa()
+        .filterNotNull()
+        .map {
+            HomeUiState(
+                listMhs = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState(
+                isLoading = true,
+            )
+        )
+}
