080401c3b840fbaec472d3f59b64d130f144d811
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2024-12-16 14:53:49 +0700

    menambahkan data class untuk menampung dan mengelola data dan fungsi untuk Mengubah Data Domain ke Data UI

--- app/src/main/java/com/example/meet9cruddengandb/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/meet9cruddengandb/ui/viewmodel/DetailMhsViewModel.kt
@@ -1,2 +1,26 @@
 package com.example.meet9cruddengandb.ui.viewmodel
 
+data class DetailUiState(
+    val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+) {
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == MahasiswaEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != MahasiswaEvent()
+}
+//data class untuk menampung data yang akan ditampilkan di UI
+
+fun Mahasiswa.toDetailUiEvent(): MahasiswaEvent {
+    return MahasiswaEvent(
+        nim = nim,
+        nama = nama,
+        jenisKelamin = jenisKelamin,
+        alamat = alamat,
+        kelas = kelas,
+        angkatan = angkatan
+    )
+}
