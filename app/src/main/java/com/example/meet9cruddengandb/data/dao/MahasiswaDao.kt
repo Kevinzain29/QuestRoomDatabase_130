ab419790d1fdc334d163549c64fc09fade32a922
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2024-12-16 14:35:21 +0700

    menambahkan fungsi untuk mengambil semua data MHS berdasarkan NIM, dan fungsi untuk delete dan update data MHS

--- app/src/main/java/com/example/meet9cruddengandb/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/meet9cruddengandb/data/dao/MahasiswaDao.kt
@@ -1,15 +1,23 @@
 package com.example.meet9cruddengandb.data.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import com.example.meet9cruddengandb.data.entity.Mahasiswa
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MahasiswaDao {
-    @Query("select * from mahasiswa")
-    fun getAllMahasiswa() : Flow<List<Mahasiswa>>
     @Insert
     suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+    @Query("SELECT * FROM mahasiswa ORDER BY nama ASC")
+    fun getAllMahasiswa(): Flow<List<Mahasiswa>>
+    @Query("SELECT * FROM mahasiswa WHERE nim = :nim")
+    fun getMahasiswa(nim: String): Flow<Mahasiswa>
+    @Delete
+    suspend fun deleteMahasiswa(mahasiswa: Mahasiswa)
+    @Update
+    suspend fun updateMahasiswa(mahasiswa: Mahasiswa)
 }
